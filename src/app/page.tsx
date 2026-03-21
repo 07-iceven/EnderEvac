5eaa25d0ab84eab3e1301a143eeb7d357eb5efbf
凌柒 <amazing.student.g@gmail.com>
2026-03-21 05:06:54 +0000

    将主界面左右两列卡片调换

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,17 +56,7 @@ export default function Home() {
 
         <div className="flex-1 p-8 max-w-7xl mx-auto w-full">
           <div className="grid grid-cols-1 lg:grid-cols-3 gap-8 items-start">
-            {/* Left Column: Configuration Cards */}
-            <div className="lg:col-span-1 space-y-8">
-              <ConfigPanel 
-                settings={settings} 
-                onUpdate={updateSettings} 
-                isDarkMode={isDarkMode} 
-                setIsDarkMode={setIsDarkMode} 
-              />
-            </div>
-
-            {/* Right Column: Dashboard and Protocol Progress (Aligned vertically) */}
+            {/* Left Column: Dashboard and Protocol Progress (Aligned vertically) */}
             <div className="lg:col-span-2 space-y-8">
               <section className="space-y-4">
                 <div className="flex justify-between items-end">
@@ -98,6 +88,16 @@ export default function Home() {
               <EvacuationProgress 
                 language={settings.language} 
                 currentStep={currentEvacStep} 
+              />
+            </div>
+
+            {/* Right Column: Configuration Cards */}
+            <div className="lg:col-span-1 space-y-8">
+              <ConfigPanel 
+                settings={settings} 
+                onUpdate={updateSettings} 
+                isDarkMode={isDarkMode} 
+                setIsDarkMode={setIsDarkMode} 
               />
             </div>
           </div>
